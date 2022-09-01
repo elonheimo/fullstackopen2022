7ae230409d9c345ab7fe0fd88017e50d8bbd0cec
Johannes Elonheimo <johannes.elonheimo@gmail.com>
2022-09-01 18:13:25 +0300

    4.2

--- osa4/blogilista/blogilista/app.js
+++ osa4/blogilista/blogilista/app.js
@@ -3,20 +3,25 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const {requestLogger, errorHandler} = require('./utils/middleware')
 const mongoose = require('mongoose')
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
-    console.log('connected to mongo DB')
+    logger.info('mongo DB connection')
   })
   .catch((error) => {
-    logger.error('error connection to MongoDB:', error.message)
+    logger.error('error while connecting to MongoDB:', error.message)
   })
 
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
+app.use(requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(errorHandler)
 
 module.exports = app
