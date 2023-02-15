063398f7c1a5613099aae61e1f392ac4effa9a4e
Johannes Elonheimo <37146266+elonheimo@users.noreply.github.com>
2023-02-15 12:06:33 +0200

    until 6.22

--- osa6/query-anecdotes/src/App.js
+++ osa6/query-anecdotes/src/App.js
@@ -1,11 +1,26 @@
-import { useQuery } from 'react-query'
+import { useMutation, useQuery, useQueryClient } from 'react-query'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { getAnecdotes } from './requests'
+import { getAnecdotes, updateAnecdote } from './requests'
 
 const App = () => {
+  
+  const queryClient = useQueryClient()
+  const updateAnecdoteMutation = useMutation(
+    updateAnecdote,
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries('anecdotes')
+      }
+    })
 
   const handleVote = (anecdote) => {
+    updateAnecdoteMutation.mutate(
+      {
+        ...anecdote,
+        votes: anecdote.votes +1
+      }
+    )
     console.log('vote')
   }
 
@@ -27,7 +42,7 @@ const App = () => {
     
       <Notification />
       <AnecdoteForm />
-      {console.log(data)}
+      {console.log('data', data)}
       {data.map(anecdote =>
         <div key={anecdote.id}>
           <div>
--- osa6/query-anecdotes/src/components/AnecdoteForm.js
+++ osa6/query-anecdotes/src/components/AnecdoteForm.js
@@ -1,11 +1,38 @@
+import { useMutation, useQueryClient } from "react-query"
+import { createAnecdote } from "../requests"
+
 const AnecdoteForm = () => {
+  
+  const queryClient = useQueryClient()
+  const newAnecdoteMutation = useMutation(
+    createAnecdote,{
+      onSuccess: (newAnecdote) => {
+        const anecdotes = queryClient.getQueryData('anecdotes')
+        queryClient.setQueryData(
+          'anecdotes',
+          anecdotes.concat(newAnecdote)
+        )
+      }}
+  )    
+
+  const asObject = (anecdote) => {
+    const getId = () => (100000 * Math.random()).toFixed(0)
+    return {
+      content: anecdote,
+      id: getId(),
+      votes: 0
+    }
+  }
 
   const onCreate = (event) => {
     event.preventDefault()
-    const content = event.target.anecdote.value
+    const newAnecdote = asObject(event.target.anecdote.value)
+    console.log(newAnecdote)
+    console.log('current anecdotes', queryClient.getQueryData('anecdotes'))
+    newAnecdoteMutation.mutate(newAnecdote)
     event.target.anecdote.value = ''
     console.log('new anecdote')
-}
+  }
 
   return (
     <div>
